5caebeb6d05f91c628311a9c5d4089268e549234
KinoMagl <88561088+KinoMagl@users.noreply.github.com>
2021-08-06 23:26:43 +0200

    Add files via upload

---
+++ client/client.lua
@@ -0,0 +1,160 @@
+-- Script By KinoMagl
+-- Scipt for Azuria FA
+-- https://github.com/KinoMagl
+
+
+--blips concess luxe
+
+Citizen.CreateThread(function()
+
+    local luxemap = AddBlipForCoord(-805.79, -225.59, 37.22)
+    SetBlipSprite(luxemap, 523)
+    SetBlipColour(luxemap, 75)
+    SetBlipScale(luxemap, 0.90)
+    SetBlipAsShortRange(luxemap, true)
+
+    BeginTextCommandSetBlipName('STRING')
+    AddTextComponentString("Concessionnaire de Luxe")
+    EndTextCommandSetBlipName(luxemap)
+
+end)
+
+Citizen.CreateThread(function()
+
+    local caca = AddBlipForCoord(2516.74, -384.15, 93.14)
+    SetBlipSprite(caca, 475)
+    SetBlipColour(caca, 63)
+    SetBlipScale(caca, 0.90)
+    SetBlipAsShortRange(caca, true)
+
+    BeginTextCommandSetBlipName('STRING')
+    AddTextComponentString("FBI")
+    EndTextCommandSetBlipName(caca)
+
+end)
+
+Citizen.CreateThread(function()
+
+    local gouv = AddBlipForCoord(-541.64, -205.94, 37.68)
+    SetBlipSprite(gouv, 419)
+    SetBlipColour(gouv, 0)
+    SetBlipScale(gouv, 0.90)
+    SetBlipAsShortRange(gouv, true)
+
+    BeginTextCommandSetBlipName('STRING')
+    AddTextComponentString("Gouvernement")
+    EndTextCommandSetBlipName(gouv)
+
+end)
+
+Citizen.CreateThread(function()
+
+    local fami = AddBlipForCoord(-26.97, -1462.77, 30.96)
+    SetBlipSprite(fami, 310)
+    SetBlipColour(fami, 2)
+    SetBlipScale(fami, 0.99)
+    SetBlipAsShortRange(fami, true)
+
+    BeginTextCommandSetBlipName('STRING')
+    AddTextComponentString("Families")
+    EndTextCommandSetBlipName(fami)
+
+end)
+
+Citizen.CreateThread(function()
+
+    local balla = AddBlipForCoord(110.25, -1939.53, 20.8)
+    SetBlipSprite(balla, 310)
+    SetBlipColour(balla, 83)
+    SetBlipScale(balla, 0.99)
+    SetBlipAsShortRange(balla, true)
+
+    BeginTextCommandSetBlipName('STRING')
+    AddTextComponentString("Ballas")
+    EndTextCommandSetBlipName(balla)
+
+end)
+
+Citizen.CreateThread(function()
+
+    local vago = AddBlipForCoord(329.58, -2029.81, 21.11)
+    SetBlipSprite(vago, 310)
+    SetBlipColour(vago, 60)
+    SetBlipScale(vago, 0.99)
+    SetBlipAsShortRange(vago, true)
+
+    BeginTextCommandSetBlipName('STRING')
+    AddTextComponentString("Vagos")
+    EndTextCommandSetBlipName(vago)
+
+end)
+
+Citizen.CreateThread(function()
+
+    local mara = AddBlipForCoord(1382.38, -1531.39, 56.88)
+    SetBlipSprite(mara, 310)
+    SetBlipColour(mara, 38)
+    SetBlipScale(mara, 0.99)
+    SetBlipAsShortRange(mara, true)
+
+    BeginTextCommandSetBlipName('STRING')
+    AddTextComponentString("Marabunta")
+    EndTextCommandSetBlipName(mara)
+
+end)
+
+Citizen.CreateThread(function()
+
+    local mede = AddBlipForCoord(-1526.94, 87.36, 56.58)
+    SetBlipSprite(mede, 310)
+    SetBlipColour(mede, 40)
+    SetBlipScale(mede, 0.99)
+    SetBlipAsShortRange(mede, true)
+
+    BeginTextCommandSetBlipName('STRING')
+    AddTextComponentString("Cartel Medellin")
+    EndTextCommandSetBlipName(mede)
+
+end)
+
+Citizen.CreateThread(function()
+
+    local medef = AddBlipForCoord(-3190.31, 800.23, 8.95)
+    SetBlipSprite(medef, 310)
+    SetBlipColour(medef, 59)
+    SetBlipScale(medef, 0.99)
+    SetBlipAsShortRange(medef, true)
+
+    BeginTextCommandSetBlipName('STRING')
+    AddTextComponentString("Koss")
+    EndTextCommandSetBlipName(medef)
+
+end)
+
+Citizen.CreateThread(function()
+
+    local medefd = AddBlipForCoord(1410.45, 1149.95, 114.23)
+    SetBlipSprite(medefd, 310)
+    SetBlipColour(medefd, 39)
+    SetBlipScale(medefd, 0.99)
+    SetBlipAsShortRange(medefd, true)
+
+    BeginTextCommandSetBlipName('STRING')
+    AddTextComponentString("Madrazo")
+    EndTextCommandSetBlipName(medefd)
+
+end)
+
+Citizen.CreateThread(function()
+
+    local medefdd = AddBlipForCoord(-2585.3, 1899.39, 171.49)
+    SetBlipSprite(medefdd, 310)
+    SetBlipColour(medefdd, 45)
+    SetBlipScale(medefdd, 0.99)
+    SetBlipAsShortRange(medefdd, true)
+
+    BeginTextCommandSetBlipName('STRING')
+    AddTextComponentString("Mafia internationale")
+    EndTextCommandSetBlipName(medefdd)
+
+end)
